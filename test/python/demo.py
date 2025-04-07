b7efad5ffa6e398e43de7c447e71160d9d023f6f
hzzhengqiwei <hzzhengqiwei@corp.netease.com>
2025-04-07 20:00:34 +0800

    提交一下新的demo

--- test/python/demo.py
+++ test/python/demo.py
@@ -1,15 +1,54 @@
 import sys
 import time
 import logging
+import json
 sys.path.append("../flowchart")
 sys.path.append("../flowchart/generate")
+sys.path.append("./")
 import asyncflow
-asyncflow.set_logger(logging)
+
+class custom_logger:
+	def info(self, msg):
+		print('----------------', msg)
+		
+	def warning(self, msg):
+		print('waring', msg)
+
+	def error(self, msg):
+		print('error', msg)
+
+	def fatal(self, msg):
+		print('fatal', msg)
+
+asyncflow.set_logger(custom_logger())
+
+asyncflow.config_log('''
+[[sink]]
+name = "rotate_out"
+type = "rotating_file_sink_st"
+base_filename = "d:/asyncflow.log"
+max_size = "1M"
+max_files = 10
+level = "info"
+                     
+[[logger]]
+name = "asyncflow"
+sinks = ["rotate_out"]
+level = "info"
+''', 'asyncflow'
+)
+
+
 asyncflow.setup({"debug_ip":u"127.0.0.1"})
 
 import asyncflow_events
-import AI
-import Subchart
+import all_flowcharts
+
+for item in all_flowcharts.file_list:
+	root = item[0]
+	file_path = item[1]
+	py_file = '../flowchart/generate/' + file_path + '.py'
+	exec(open(py_file).read())
 
 print(asyncflow.EventId.Tick)
 
@@ -24,6 +63,7 @@ class Character:
 
     def Say(self, s):
         print(s)
+        raise Exception("haha")
         #test()
         #self.output.append(str(s))
 
@@ -35,8 +75,8 @@ class Character:
         asyncflow.start(char)
         return char
 
-if __name__ == "__main__":
-    chart_name = "AI.test_04"
+def test_hotfix():
+    chart_name = "AI.test_03"
 
     # start
     c = Character()
@@ -44,10 +84,50 @@ if __name__ == "__main__":
     asyncflow.attach(c, chart_name)
     asyncflow.start(c)
 
+    # loop
+    for i in range(40):
+        time.sleep(0.1)
+        asyncflow.step(100)
+    # destroy
+    asyncflow.remove(c, chart_name)
 
+    asyncflow.attach(c, chart_name)
+    asyncflow.start(c)
+
+    for i in range(40):
+        time.sleep(0.1)
+        asyncflow.step(100)
+
+    
+    asyncflow.deregister(c)
+    asyncflow.step(10)
+    asyncflow.exit()
+
+def test():
+    chart_name = "AI.test_01"
+
+    for i in range(100):
+        #time.sleep(0.1)
+        asyncflow.step(100)
+
+    # start
+    c = Character()
+    asyncflow.register(c)
+    asyncflow.attach(c, chart_name)
+    asyncflow.start(c)
 
     # loop
-    for i in range(10000):
+    for i in range(40000000):
+        time.sleep(0.1)
+        asyncflow.step(100)
+        if i % 30 == 0:
+	        for j in range(5):
+		        asyncflow.event(c, asyncflow.EventId.CustomEvent)
+        #if i%10 == 0:
+	    #    print(json.dumps(asyncflow.dump(c), indent=4))
+
+    # loop
+    for i in range(1):
         time.sleep(0.1)
         asyncflow.step(100)
 
@@ -56,3 +136,7 @@ if __name__ == "__main__":
     asyncflow.deregister(c)
     asyncflow.step(10)
     asyncflow.exit()
+
+
+if __name__ == "__main__":
+	test_hotfix()
